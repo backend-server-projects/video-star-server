de34f8b3f4d8c77007bcf078919752a8db552a72
Arman Khan <sparmankhan@gmail.com>
2023-03-12 22:49:34 +0600

    mongodb user data added

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://video-star.vercel.app",
+    origin: "http://localhost:5173",
     methods: ["GET", "POST", "UPDATE", "DELETE"],
   },
 });
